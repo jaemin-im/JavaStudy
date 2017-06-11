7cdb6c43e2598e273a354f4b0cda727a7b09dcb9
Xsolute <Xsolute01@gmail.com>
2017-06-11 23:06:24 +0900

    final 필드 선언과 초기화

---
+++ src/javaStudy/PeopleExample.java
@@ -0,0 +1,18 @@
+package javaStudy;
+
+/**
+ * Created by dsm2017 on 2017-06-11.
+ */
+public class PeopleExample {
+    public static void main(String[] args) {
+        People p1 = new People("123456-1234567", "계백");
+
+        System.out.println(p1.nation);
+        System.out.println(p1.ssn);
+        System.out.println(p1.name);
+
+        //p1.nation = "usa";
+        //p1.ssn = "654321-77654321";
+        p1.name = "을지문덕"; // final 필드는 값 수정 불가
+    }
+}
