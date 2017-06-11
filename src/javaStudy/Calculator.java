89aa9efa9ea783f48d7b95195c5a998a76b9faed
Xsolute <Xsolute01@gmail.com>
2017-06-11 22:13:46 +0900

    클래스 내부에서 메소드 호출

--- src/javaStudy/Calculator.java
+++ src/javaStudy/Calculator.java
@@ -4,21 +4,23 @@ package javaStudy;
  * Created by dsm2017 on 2017-06-11.
  */
 public class Calculator {
-    void powerOn() {
-        System.out.println("전원을 켭니다.");
-    }
-
     int plus(int x, int y) {
-        int result = x + y;
+        int result = x+y;
         return result;
     }
 
-    double divide(int x, int y) {
-        double result = (double)x / (double)y;
+    double avg(int x, int y) {
+        double sum = plus(x,y);
+        double result = sum / 2;
         return result;
     }
 
-    void powerOff() {
-        System.out.println("전원을 끕니다.");
+    void execute() {
+        double result = avg(7,10);
+        println("실행결과 : " + result);
+    }
+
+    void println(String message) {
+        System.out.println(message);
     }
 }
