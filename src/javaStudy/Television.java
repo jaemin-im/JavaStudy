a94d8caf3a1d12a5e0723a646a8faa41c315c8da
Xsolute <Xsolute01@gmail.com>
2017-06-11 22:37:08 +0900

    정적 초기화 블록

---
+++ src/javaStudy/Television.java
@@ -0,0 +1,14 @@
+package javaStudy;
+
+/**
+ * Created by dsm2017 on 2017-06-11.
+ */
+public class Television {
+    static String company = "Samsung";
+    static String model = "LCD";
+    static String info;
+
+    static {
+        info = company + "-" + model;
+    }
+}
---
+++ src/javaStudy/TelevisionExample.java
@@ -0,0 +1,10 @@
+package javaStudy;
+
+/**
+ * Created by dsm2017 on 2017-06-11.
+ */
+public class TelevisionExample {
+    public static void main(String[] args) {
+        System.out.println(Television.info);
+    }
+}
