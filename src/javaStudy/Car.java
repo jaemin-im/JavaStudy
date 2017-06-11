4b51e6494658516a066b884f7248e0032c9d078d
Xsolute <Xsolute01@gmail.com>
2017-06-11 22:24:32 +0900

    인스턴스 멤버와 this

--- src/javaStudy/Car.java
+++ src/javaStudy/Car.java
@@ -4,27 +4,21 @@ package javaStudy;
  * Created by dsm2017 on 2017-06-11.
  */
 public class Car {
-    // 필드 - Field
-    String company = "현대자동차";
-    String model = "그랜저";
-    String color = "검정";
-    int maxSpeed = 350;
+    String model;
     int speed;
 
-    // 생성자 - Constructor
-    Car() {}
-
     Car(String model) {
-        this(model, "은색", 250);
-    }
-
-    Car(String model, String color) {
-        this(model, color, 250);
-    }
-
-    Car(String model, String color, int maxSpeed) {
         this.model = model;
-        this.color = color;
-        this.maxSpeed = maxSpeed;
+    }
+
+    void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    void run() {
+        for(int i=10;i<=50;i+=10) {
+            this.setSpeed(i);
+            System.out.println(this.model + "가 달립니다.(시속 : " + this.speed + "km/h)");
+        }
     }
 }
