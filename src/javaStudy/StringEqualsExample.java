c468d998a92ceeaa451af20d90d813cdf49b9781
Xsolute <Xsolute01@gmail.com>
2017-06-11 09:47:23 +0900

    String 객체비교 및 내용비교

---
+++ src/javaStudy/StringEqualsExample.java
@@ -0,0 +1,35 @@
+package javaStudy;
+
+/**
+ * Created by dsm2017 on 2017-06-10.
+ */
+
+public class StringEqualsExample {
+    public static void main(String args[]) {
+        String strVar1 = "신민철";
+        String strVar2 = "신민철";
+
+        if(strVar1 == strVar2) {
+            System.out.println("strVar1과 strVar2는 참조가 같음");
+        } else {
+            System.out.println("strVar1과 strVar2는 참조가 다름");
+        }
+
+        if(strVar1.equals(strVar2)) {
+            System.out.println("strVar1과 strVar2는 문자열이 같음");
+        }
+
+        String strVar3 = new String("신민철");
+        String strVar4 = new String("신민철");
+
+        if(strVar3 == strVar4) {
+            System.out.println("strVar3과 strVar4는 참조가 같음");
+        } else {
+            System.out.println("strVar3과 strVar4는 참조가 다름");
+        }
+
+        if(strVar3.equals(strVar4)) {
+            System.out.println("strVal3과 strVar4는 문자열이 같음");
+        }
+    }
+}
