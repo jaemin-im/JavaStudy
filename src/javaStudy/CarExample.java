abec0e7ea9400eb0be635aebaa8c206b6b0b8b4a
Xsolute <Xsolute01@gmail.com>
2017-06-12 22:59:35 +0900

    하나의 배열로 객체 관리

--- src/javaStudy/CarExample.java
+++ src/javaStudy/CarExample.java
@@ -9,24 +9,9 @@ public class CarExample {
 
         for(int i=1;i<=5;i++) {
             int problemLocation = car.run();
-
-            switch(problemLocation) {
-                case 1:
-                    System.out.println("앞왼쪽 HankookTire로 교체");
-                    car.frontLeftTire = new HankookTire("앞왼쪽", 15);
-                    break;
-                case 2:
-                    System.out.println("앞오른쪽 KumhoTire로 교체");
-                    car.frontLeftTire = new HankookTire("앞오른쪽", 13);
-                    break;
-                case 3:
-                    System.out.println("뒤왼쪽 HankookTire로 교체");
-                    car.frontLeftTire = new HankookTire("뒤왼쪽", 14);
-                    break;
-                case 4:
-                    System.out.println("뒤오른쪽 HankookTire로 교체");
-                    car.frontLeftTire = new HankookTire("뒤오른쪽", 17);
-                    break;
+            if(problemLocation != 0) {
+                System.out.println(car.tires[problemLocation-1].location + " HankookTire로 교체");
+                car.tires[problemLocation-1] = new HankookTire(car.tires[problemLocation-1].location, 15);
             }
             System.out.println("-------------------------------");
         }
