ff32abff836016a4e75464a8157cb8d0acaf76f7
Xsolute <Xsolute01@gmail.com>
2017-06-13 20:23:45 +0900

    객체 동등 비교(equals() 메소드)

---
+++ src/javaStudy/Member.java
@@ -0,0 +1,22 @@
+package javaStudy;
+
+/**
+ * Created by dsm2017 on 2017-06-13.
+ */
+public class Member {
+    public String id;
+    public Member(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Member) {
+            Member member = (Member) obj;
+            if(id.equals(member.id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
