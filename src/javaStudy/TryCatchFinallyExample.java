76681a3d021ba6e4127c7441a865bb1b5e231ba6
Xsolute <Xsolute01@gmail.com>
2017-06-13 14:27:28 +0900

    (다중) 예외 처리

---
+++ src/javaStudy/TryCatchFinallyExample.java
@@ -0,0 +1,14 @@
+package javaStudy;
+
+/**
+ * Created by dsm2017 on 2017-06-13.
+ */
+public class TryCatchFinallyExample {
+    public static void main(String[] args) {
+        try {
+            Class clazz = Class.forName("java.lang.String2");
+        } catch (ClassNotFoundException e) {
+            System.out.println("클래스가 존재하지 않습니다.");
+        }
+    }
+}
