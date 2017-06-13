c1266c38e2ab5e27b54ebea2614ff87c887a1cce
Xsolute <Xsolute01@gmail.com>
2017-06-13 14:55:21 +0900

    AutoCloseable 구현 클래스

---
+++ src/javaStudy/TryWithResourceExample.java
@@ -0,0 +1,15 @@
+package javaStudy;
+
+/**
+ * Created by dsm2017 on 2017-06-13.
+ */
+public class TryWithResourceExample {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("file.txt")) {
+            fis.read();
+            throw new Exception();
+        } catch (Exception e) {
+            System.out.println("예외 처리 코드가 실행되었습니다.");
+        }
+    }
+}
