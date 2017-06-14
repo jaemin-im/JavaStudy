03788d2e0a07b7da81c54181e4c262a6ea9a846a
Xsolute <Xsolute01@gmail.com>
2017-06-14 23:37:15 +0900

    스레드 yield()

--- src/javaStudy/ThreadA.java
+++ src/javaStudy/ThreadA.java
@@ -4,12 +4,17 @@ package javaStudy;
  * Created by dsm2017 on 2017-06-14.
  */
 public class ThreadA extends Thread {
-    public ThreadA() {
-        setName("ThreadA");
-    }
+    public boolean stop = false; // 종료 플래그
+    public boolean work = true; // 작업 진행 여부 플래그
+
     public void run() {
-        for(int i=0;i<2;i++) {
-            System.out.println(getName() + "가 출력한 내용");
+        while (!stop) {
+            if(work) {
+                System.out.println("ThreadA 작업 내용");
+            } else {
+                Thread.yield();
+            }
         }
+        System.out.println("ThreadA 종료");
     }
 }
--- src/javaStudy/ThreadB.java
+++ src/javaStudy/ThreadB.java
@@ -4,9 +4,17 @@ package javaStudy;
  * Created by dsm2017 on 2017-06-14.
  */
 public class ThreadB extends Thread {
+    public boolean stop = false; // 종료 플래그
+    public boolean work = true; // 작업 진행 여부 플래그
+
     public void run() {
-        for(int i=0;i<2;i++) {
-            System.out.println(getName() + "가 출력한 내용");
+        while (!stop) {
+            if(work) {
+                System.out.println("ThreadB 작업 내용");
+            } else {
+                Thread.yield();
+            }
         }
+        System.out.println("ThreadB 종료");
     }
 }
