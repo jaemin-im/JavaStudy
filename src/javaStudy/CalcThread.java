1ae472deb941aae702691051ffb96058d5067caf
Xsolute <Xsolute01@gmail.com>
2017-06-14 22:23:20 +0900

    우선순위를 설정해서 스레드 실행

---
+++ src/javaStudy/CalcThread.java
@@ -0,0 +1,15 @@
+package javaStudy;
+
+/**
+ * Created by dsm2017 on 2017-06-14.
+ */
+public class CalcThread extends Thread {
+    public CalcThread(String name) {
+        setName(name);
+    }
+    public void run() {
+        for(int i=0;i<2000000000;i++) {
+        }
+        System.out.println(getName());
+    }
+}
---
+++ src/javaStudy/PriorityExample.java
@@ -0,0 +1,18 @@
+package javaStudy;
+
+/**
+ * Created by dsm2017 on 2017-06-14.
+ */
+public class PriorityExample {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++) {
+            Thread thread = new CalcThread("thread" + i);
+            if(i!=10) {
+                thread.setPriority(Thread.MIN_PRIORITY);
+            } else {
+                thread.setPriority(Thread.MAX_PRIORITY);
+            }
+            thread.start();
+        }
+    }
+}
