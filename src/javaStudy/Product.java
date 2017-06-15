22108b3cad850568d7513bb73f44c516b4e85bfe
Xsolute <Xsolute01@gmail.com>
2017-06-15 19:42:40 +0900

    멀티 타입 파라미터 class<K,V,...>

---
+++ src/javaStudy/Product.java
@@ -0,0 +1,15 @@
+package javaStudy;
+
+/**
+ * Created by dsm2017 on 2017-06-15.
+ */
+public class Product<T, M> {
+    private T kind;
+    private M model;
+
+    public T getKind() { return this.kind; }
+    public M getModel() { return this.model; }
+
+    public void setKind(T kind) { this.kind = kind; }
+    public void setModel(M model) { this.model = model; }
+}
