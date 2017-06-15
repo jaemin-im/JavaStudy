bff364a46bac8bc5b85abb26e2c730a33f014745
Xsolute <Xsolute01@gmail.com>
2017-06-15 20:22:31 +0900

    제한된 타입 파라미터 <T extends 최상위타입>

---
+++ src/javastudy/BoundedTypeParameterExample.java
@@ -0,0 +1,11 @@
+public class BoundedTypeParameterExample {
+    public static void main(String[] args) {
+        // String str = Util.compare("a", "b"); <- String은 Number 타입이 아님
+
+        int result1 = Util.compare(10, 20); // int->Integer 자동 Boxing
+        System.out.println(result1);
+
+        int result2 = Util.compare(4.5, 3); // double->Double 자동 Boxing
+        System.out.println(result2);
+    }
+}
