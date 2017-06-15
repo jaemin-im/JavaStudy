317e95cb8e05e552b0a3d7f25c3e1d31de01d26c
Xsolute <Xsolute01@gmail.com>
2017-06-15 19:57:40 +0900

    제네릭 메소드 <K,V> T method()

--- src/javaStudy/BoxingMethodExample.java
+++ src/javaStudy/BoxingMethodExample.java
@@ -3,7 +3,7 @@ package javaStudy;
 /**
  * Created by dsm2017 on 2017-06-15.
  */
-public class BoxingMethodExample {
+/*public class BoxingMethodExample {
     public static void main(String[] args) {
         Box<Integer> box1 = Util.<Integer>boxing(100);
         int intValue = box1.get();
@@ -11,4 +11,4 @@ public class BoxingMethodExample {
         Box<String> box2 = Util.boxing("홍길동");
         String strValue = box2.get();
     }
-}
+}*/
--- src/javaStudy/Util.java
+++ src/javaStudy/Util.java
@@ -4,9 +4,9 @@ package javaStudy;
  * Created by dsm2017 on 2017-06-15.
  */
 public class Util {
-    public static <T> Box<T> boxing(T t) {
-        Box<T> box = new Box<T>();
-        box.set(t);
-        return box;
+    public static <K, V> boolean compare(Pair <K,V> p1, Pair<K,V> p2) {
+        boolean keyCompare = p1.getKey().equals(p2.getKey());
+        boolean valueCompare = p1.getValue().equals(p2.getValue());
+        return keyCompare && valueCompare;
     }
 }
